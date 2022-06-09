95067e92663d17b89410f97527008f1824729591
antoine-granger <antoine77.granger@outlook.fr>
2022-06-09 13:15:05 +0200

    Add dynamic size for our grid

--- TP5/snake2020/grid.h
+++ TP5/snake2020/grid.h
@@ -7,18 +7,19 @@
 #define NBC 36
 
 typedef struct {
-    char** grid;
+    char** char_grid;
     int nbl;
     int nbc;
 } Grid;
 enum  Element{WALL = 'w', EMPTY = '-', FRUIT = 'f', SNAKE = 's'};
-float compute_size(int h, int w);
+float compute_size(Grid * struct_grid, int h, int w);
 void debug(Grid *struct_grid);
 void draw_grid(Grid *struct_grid);
 int get_grid(Grid *struct_grid, const char file_name[]);
+void get_grid_size(Grid *struct_grid, const char file_name[]);
 enum Element move_snake(Grid *struct_grid, Snake *snake);
 void place_snake(Grid *struct_grid, Snake *snake);
-Grid* allocate_grid(int n, int m);
+Grid* allocate_grid(const char *file_name);
 void free_grid(Grid *);
 
 #endif
